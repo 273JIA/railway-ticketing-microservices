8e28ab345ea0b8faaf291a6d82af0c79b0e33090
Jia273 <114813337+01226@users.noreply.github.com>
2024-07-27 17:53:20 +0100

    save passenger information

---
+++ member/src/main/java/com/jiawa/train/member/service/PassengerService.java
@@ -0,0 +1,38 @@
+package com.jiawa.train.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.jiawa.train.common.context.LoginMemberContext;
+import com.jiawa.train.common.util.SnowUtil;
+import com.jiawa.train.member.domain.Passenger;
+import com.jiawa.train.member.mapper.PassengerMapper;
+import com.jiawa.train.member.req.PassengerSaveReq;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PassengerService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PassengerService.class);
+
+    @Resource
+    private PassengerMapper passengerMapper;
+
+    public void save(PassengerSaveReq req) {
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        if (ObjectUtil.isNull(passenger.getId())) {
+            passenger.setMemberId(LoginMemberContext.getId());
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerMapper.insert(passenger);
+        } else {
+            passenger.setUpdateTime(now);
+            passengerMapper.updateByPrimaryKey(passenger);
+        }
+    }
+}
