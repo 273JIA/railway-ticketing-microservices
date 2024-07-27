29a79180c38b5e4e612328eacbe6c545a34b2b4d
Jia273 <114813337+01226@users.noreply.github.com>
2024-07-27 17:01:54 +0100

    add passenger

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -18,19 +18,19 @@ public class MemberController {
     private MemberService memberService;
 
     @GetMapping("/count")
-    public CommonResp<Integer> count(){
+    public CommonResp<Integer> count() {
         int count = memberService.count();
-//        CommonResp<Integer> commonResp = new CommonResp();
-//        commonResp.setContent(count);
-//        return commonResp;
-        return new CommonResp<>(count);
+        CommonResp<Integer> commonResp = new CommonResp<>();
+        commonResp.setContent(count);
+        return commonResp;
     }
+
     @PostMapping("/register")
-    public CommonResp<Long> register(@Valid MemberRegisterReq req){
+    public CommonResp<Long> register(@Valid MemberRegisterReq req) {
         long register = memberService.register(req);
-//        CommonResp<Long> commonResp = new CommonResp<>();
-//        commonResp.setContent(register);
-//        return commonResp;
+        // CommonResp<Long> commonResp = new CommonResp<>();
+        // commonResp.setContent(register);
+        // return commonResp;
         return new CommonResp<>(register);
     }
 
