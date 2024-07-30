285432dd6baa17391627add901decff5859649ef
Jia273 <114813337+01226@users.noreply.github.com>
2024-07-30 16:20:36 +0100

    add quartz

--- batch/src/main/java/com/jiawa/train/batch/config/BatchApplication.java
+++ batch/src/main/java/com/jiawa/train/batch/config/BatchApplication.java
@@ -1,16 +1,18 @@
 package com.jiawa.train.batch.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jiawa")
-//@MapperScan("com.jiawa.train.*.mapper")
-//@EnableFeignClients("com.jiawa.train.batch.feign")
+@MapperScan("com.jiawa.train.*.mapper")
+@EnableFeignClients("com.jiawa.train.batch.feign")
 public class BatchApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
