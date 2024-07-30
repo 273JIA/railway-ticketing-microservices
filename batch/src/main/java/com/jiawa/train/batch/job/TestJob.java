c6124c09b7e82ee0f8f35757e8b5419b9bb9e4e2
Jia273 <114813337+01226@users.noreply.github.com>
2024-07-30 15:38:44 +0100

    add quartz

---
+++ batch/src/main/java/com/jiawa/train/batch/job/TestJob.java
@@ -0,0 +1,21 @@
+package com.jiawa.train.batch.job;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+@DisallowConcurrentExecution
+public class TestJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("TestJob TEST开始");
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        System.out.println("TestJob TEST结束");
+    }
+}
